f8b5a55aff2bdd9ffaa9e9aa91a59fe53ab6cead
brickwalker <artem.nedostup@gmail.com>
2022-01-10 19:11:11 +0000

    implement push on linked list

--- linkedlist.js
+++ linkedlist.js
@@ -7,13 +7,29 @@ class Node {
 
 class LinkedList {
   constructor(value) {
-    const myNode = new Node(value);
-    this.head = myNode;
+    const baseNode = new Node(value);
+    this.head = baseNode;
     this.tail = this.head;
     this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++
   }
 }
 
 const myLinkedList = new LinkedList(4);
 
-console.log(myLinkedList);
+console.log("NEW", myLinkedList);
+
+myLinkedList.push(77);
+
+console.log("PUSH", myLinkedList);
