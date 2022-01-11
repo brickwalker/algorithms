e7a6457c5affedd0d4a33c58ac038bf97af64677
brickwalker <artem.nedostup@gmail.com>
2022-01-11 12:22:52 +0000

    add push

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -13,8 +13,23 @@ class DoublyLinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    const node = new DoublyNode(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const dLinkList = new DoublyLinkedList(1);
 
 console.log(dLinkList);
+console.log(dLinkList.push(2));
