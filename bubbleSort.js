4d2455ea1d8f87f2081fedf7e46998c5a6e48f05
brickwalker <artem.nedostup@gmail.com>
2022-01-13 10:20:09 +0000

    update bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,5 @@
 
-const selectionSort = (array) => {
+const bubbleSort = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
         for (let j = 0; j < i; j++) {
             if (array[j + 1] < array[j]) {
@@ -12,6 +12,6 @@ const selectionSort = (array) => {
     return array;
 }
 
-console.log(selectionSort([5, 2, 1, 3]));
-console.log(selectionSort([2, 1]));
-console.log(selectionSort([1]));
+console.log(bubbleSort([5, 2, 1, 3]));
+console.log(bubbleSort([2, 1]));
+console.log(bubbleSort([1]));
